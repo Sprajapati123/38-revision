d529a72fc3b59762fc2a8b70acf277420cea7dbb
Sprajapati123 <sandisprajapati52@gmail.com>
2026-06-14 11:54:33 +0545

    viewmodel

--- app/src/main/java/com/example/c38re/view/AddProductActivity.kt
+++ app/src/main/java/com/example/c38re/view/AddProductActivity.kt
@@ -4,11 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -17,8 +23,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.c38re.view.ui.theme.C38reTheme
 
 class AddProductActivity : ComponentActivity() {
@@ -43,13 +52,77 @@ fun AddProductBody() {
         modifier = Modifier.fillMaxSize()
     ) {
         item {
+
+            Spacer(modifier = Modifier.height(25.dp))
             OutlinedTextField(
                 modifier = Modifier.fillMaxWidth(),
                 value = name,
+                placeholder = {
+                    Text("Enter Product Name")
+                },
                 onValueChange = {
                     name = it
                 }
             )
+            Spacer(modifier = Modifier.height(15.dp))
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = price,
+                placeholder = {
+                    Text("Enter Product price")
+                },
+                onValueChange = {
+                    price = it
+                }
+            )
+
+            Spacer(modifier = Modifier.height(15.dp))
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = description,
+                placeholder = {
+                    Text("Enter Product description")
+                },
+                onValueChange = {
+                    description = it
+                }
+            )
+
+            Spacer(modifier = Modifier.height(15.dp))
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = quantity,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number
+                ),
+                placeholder = {
+                    Text("Enter Product quantity")
+                },
+                onValueChange = {
+                    quantity = it
+                }
+            )
+
+            Spacer(modifier = Modifier.height(15.dp))
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Checkbox(
+                    checked = isActive,
+                    onCheckedChange = {
+                        isActive = it
+                    }
+                )
+                Text("Is Active")
+            }
+
+            Spacer(modifier = Modifier.height(15.dp))
+
+            ElevatedButton(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = {}) {
+                Text("Add Product")
+            }
         }
     }
 }
